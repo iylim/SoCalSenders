5885afd3f60e6939e56c5f9445f50561412c5441
Ivy Lim <yunjilim@gmail.com>
2018-07-20 09:51:18 -0700

    no notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     after_initialize :set_defaults
     has_secure_password
     has_many :bookmarks, dependent: :destroy
-    # has_many :favorites, through: :bookmarks, source: :routes
     has_many :pictures, dependent: :destroy
     has_many :ratings, dependent: :destroy
     has_many :comments,  dependent: :destroy
